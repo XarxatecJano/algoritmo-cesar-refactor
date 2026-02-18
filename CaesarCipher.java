7fb89b4202b7d7d0686a8b2e691ac937713c2821
Jesus Martinez <jesus.martinez@cuatroochenta.com>
2026-02-18 23:24:09 +0100

    Refactor old CaesarCipher with encapsulated logic delegating ciphering to TextShifter

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -14,76 +14,43 @@ según lo visto en la sesión de Clean Code
 
 public class CaesarCipher {
     
-    private static final int ALPHABET_LENGTH = 26;
-    
-    private static class Letters {
-        static final int A = 65;
-        static final int Z = 90;
-        static final int a = 97;
-        static final int z = 122;
-    }
-    
-    private static boolean isUpperCaseLetterOutOfRange(int charCode, int shift) {
-        return charCode >= Letters.A && charCode <= Letters.Z && 
-               (charCode + shift > Letters.Z || charCode - shift < Letters.A);
-    }
-    
-    private static boolean isLowerCaseOutOfRange(int charCode, int shift) {
-        return charCode >= Letters.a && charCode <= Letters.z && 
-               (charCode + shift > Letters.z || charCode - shift < Letters.a);
-    }
-    
-    private static boolean isOutOfAlphabet(int charCode, int shift) {
-        return isUpperCaseLetterOutOfRange(charCode, shift) || 
-               isLowerCaseOutOfRange(charCode, shift);
-    }
-    
-    public static String cipher(String text, int shift) {
-        StringBuilder cipher = new StringBuilder();
-        char newCharToAddToCipher;
-        int shiftToApply, currentChar;
-        shift = shift % ALPHABET_LENGTH;
+    private final TextShifter textShifter;
+
+    public CaesarCipher() {
+
+        ShiftNormalizer normalizer = new ShiftNormalizer(26);
+
+        CharShifter charShifter = new CharShifter(
+            AlphabetRange.uppercase(),
+            AlphabetRange.lowercase(),
+            NonLetterPolicy.shifNonLetterPolicy()
+        );
         
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = (int) text.charAt(i);
-            shiftToApply = isOutOfAlphabet(currentChar, shift) ? 
-                          shift - ALPHABET_LENGTH : shift;
-            newCharToAddToCipher = (char) (currentChar + shiftToApply);
-            cipher.append(newCharToAddToCipher);
-        }
-        return cipher.toString();
+        this.textShifter = new TextShifter(normalizer, charShifter);
     }
-    
-    public static String decipher(String text, int shift) {
-        StringBuilder decipher = new StringBuilder();
-        char newCharToAddToDecipher;
-        int shiftToApply, currentChar;
-        shift = -shift % ALPHABET_LENGTH;
-        
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = (int) text.charAt(i);
-            shiftToApply = isOutOfAlphabet(currentChar, shift) ? 
-                          shift + ALPHABET_LENGTH : shift;
-            newCharToAddToDecipher = (char) (currentChar + shiftToApply);
-            decipher.append(newCharToAddToDecipher);
-        }
-        return decipher.toString();
+
+    public String cipher(String text, int shift) {
+        return textShifter.shift(text, shift);
     }
-    
-    public static void main(String[] args) {
-        // Test 1
-        String result1 = cipher("Hello World", 1);
+
+    public String decipher(String text, int shift){
+        return textShifter.shift(text, -shift);
+    }
+
+    public static void main (String[] args){
+
+        CaesarCipher cipher = new CaesarCipher();
+
+        String result1 = cipher.cipher("Hello World", 1);
         String expected1 = "Ifmmp!Xpsme";
-        assert result1.equals(expected1) : 
-            String.format("%s === '%s'", result1, expected1);
-        
-        // Test 2
-        String ciphered = cipher("Hello World", 3);
-        String result2 = decipher(ciphered, 3);
+        assert result1.equals(expected1);
+
+        String ciphered = cipher.cipher("Hello World", 3);
+        String result2 = cipher.decipher(ciphered, 3);
         String expected2 = "Hello World";
-        assert result2.equals(expected2) : 
-            String.format("%s === '%s'", result2, expected2);
-        
-        System.out.println("Todos los tests han pasado correctamente");
+        assert result2.equals(expected2);
+
+        System.out.println("Funcionó!!!");
     }
+    
 }
