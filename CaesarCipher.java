4ee15a24e9fa8f9ab37602c32cc25fb864b0b2cb
bri3ll3 <biacra2@alu.edu.gva.es>
2026-02-19 15:27:10 +0100

    refactor: deleted comments

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -1,17 +1,3 @@
-/*
-El Cifrado César es una de las técnicas de cifrado más simples y conocidas. 
-Se trata de un tipo de cifrado de sustitución en el que cada letra del texto sin cifrar es reemplazada por otra letra 
-que se encuentra un número fijo de posiciones hacia abajo en el alfabeto. 
-Por ejemplo, con un desplazamiento hacia la derecha de 3, la letra E sería reemplazada por H, 
-la F se convertiría en I, y así sucesivamente.
-Esta transformación se puede representar alineando dos alfabetos: el alfabeto cifrado es el alfabeto normal 
-rotado hacia la derecha un cierto número de posiciones.
-
-A continuación tienes dos funciones que codifican y decodifican usando el cifrado César.
-Tu tarea consiste en entender el código y refactorizarlo para que sea lo más limpio posible, 
-según lo visto en la sesión de Clean Code
-*/
-
 public class CaesarCipher {
     
     private static final int ALPHABET_LENGTH = 26;
