c6ae9ba6d52d33d794aff11f4112836399bbfaf7
Ivan Ribes Moliner <ivan.ribes@cuatroochenta.com>
2026-03-27 08:08:46 +0100

    Ejercicio Clean Code Ivan Ribés

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -13,77 +13,80 @@ según lo visto en la sesión de Clean Code
 */
 
 public class CaesarCipher {
-    
+
     private static final int ALPHABET_LENGTH = 26;
-    
-    private static class Letters {
-        static final int A = 65;
-        static final int Z = 90;
-        static final int a = 97;
-        static final int z = 122;
-    }
-    
-    private static boolean isUpperCaseLetterOutOfRange(int charCode, int shift) {
-        return charCode >= Letters.A && charCode <= Letters.Z && 
-               (charCode + shift > Letters.Z || charCode - shift < Letters.A);
-    }
-    
-    private static boolean isLowerCaseOutOfRange(int charCode, int shift) {
-        return charCode >= Letters.a && charCode <= Letters.z && 
-               (charCode + shift > Letters.z || charCode - shift < Letters.a);
-    }
-    
-    private static boolean isOutOfAlphabet(int charCode, int shift) {
-        return isUpperCaseLetterOutOfRange(charCode, shift) || 
-               isLowerCaseOutOfRange(charCode, shift);
-    }
-    
-    public static String cipher(String text, int shift) {
-        StringBuilder cipher = new StringBuilder();
-        char newCharToAddToCipher;
-        int shiftToApply, currentChar;
-        shift = shift % ALPHABET_LENGTH;
-        
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = (int) text.charAt(i);
-            shiftToApply = isOutOfAlphabet(currentChar, shift) ? 
-                          shift - ALPHABET_LENGTH : shift;
-            newCharToAddToCipher = (char) (currentChar + shiftToApply);
-            cipher.append(newCharToAddToCipher);
-        }
-        return cipher.toString();
-    }
-    
-    public static String decipher(String text, int shift) {
-        StringBuilder decipher = new StringBuilder();
-        char newCharToAddToDecipher;
-        int shiftToApply, currentChar;
-        shift = -shift % ALPHABET_LENGTH;
-        
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = (int) text.charAt(i);
-            shiftToApply = isOutOfAlphabet(currentChar, shift) ? 
-                          shift + ALPHABET_LENGTH : shift;
-            newCharToAddToDecipher = (char) (currentChar + shiftToApply);
-            decipher.append(newCharToAddToDecipher);
-        }
-        return decipher.toString();
-    }
-    
+
     public static void main(String[] args) {
         // Test 1
         String result1 = cipher("Hello World", 1);
         String expected1 = "Ifmmp!Xpsme";
-        assert result1.equals(expected1) : 
-            String.format("%s === '%s'", result1, expected1);
-        
+        assert  result1.equals(expected1) :
+                String.format("%s === '%s'", result1, expected1);
+
         // Test 2
         String ciphered = cipher("Hello World", 3);
         String result2 = decipher(ciphered, 3);
         String expected2 = "Hello World";
-        assert result2.equals(expected2) : 
-            String.format("%s === '%s'", result2, expected2);
-        
+        assert result2.equals(expected2) :
+                String.format("%s === '%s'", result2, expected2);
+
         System.out.println("Todos los tests han pasado correctamente");
+    }
+
+    public static String cipher(String text, int shift) {
+        shift = calculateShift(shift);
+        return shiftCharacters(text, shift);
+    }
+
+    public static String decipher(String text, int shift) {
+        shift = calculateShift(shift);
+        return  shiftCharacters(text, -shift);
+    }
+
+    private static String shiftCharacters(String text, int shift) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int index = 0; index < text.length(); index++) {
+            char currentChar = text.charAt(index);
+            stringBuilder.append(transformChar(currentChar, shift));
+        }
+
+        return stringBuilder.toString();
+    }
+
+    private static char transformChar(char currentChar, int shift) {
+        char newChar;
+
+        if (Character.isUpperCase(currentChar)) {
+            newChar = rotateChar(currentChar, shift, 'A', 'Z');
+        } else if (Character.isLowerCase(currentChar)){
+            newChar = rotateChar(currentChar, shift, 'a', 'z');
+        } else {
+            newChar = (char)(((int)currentChar) + shift);
+        }
+
+        return newChar;
+    }
+
+    private static char rotateChar(char currentChar, int shift, int min, int max) {
+
+        char newAsciiCode = (char)((int)currentChar + shift);
+
+        if (newAsciiCode > max) {
+            newAsciiCode -= ALPHABET_LENGTH;
+        } else if (newAsciiCode < min) {
+            newAsciiCode += ALPHABET_LENGTH;
+        }
+
+        return newAsciiCode;
+    }
+
+    private static int calculateShift(int shift) {
+        if (shift < -ALPHABET_LENGTH) {
+            shift = (shift % ALPHABET_LENGTH) + ALPHABET_LENGTH;
+        } else if (shift > ALPHABET_LENGTH){
+            shift = shift%ALPHABET_LENGTH;
+        }
+        return shift;
     }
 }
