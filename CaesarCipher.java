4726bbd67507ed8c741c03dfd76c72b820e79ffb
carlosalbillos <carlos.albillos@cuatroochenta.com>
2026-03-28 14:43:38 +0100

    carlos-refactorizado

--- CaesarCipher.java
+++ CaesarCipher.java
@@ -13,77 +13,78 @@ según lo visto en la sesión de Clean Code
 */
 
 public class CaesarCipher {
-    
+
     private static final int ALPHABET_LENGTH = 26;
-    
-    private static class Letters {
-        static final int A = 65;
+
+    private static class ASCII {
+        static final int A = 65; 
         static final int Z = 90;
-        static final int a = 97;
+        static final int a = 97; 
         static final int z = 122;
     }
-    
-    private static boolean isUpperCaseLetterOutOfRange(int charCode, int shift) {
-        return charCode >= Letters.A && charCode <= Letters.Z && 
-               (charCode + shift > Letters.Z || charCode - shift < Letters.A);
+
+    private static boolean isUpperCaseLetter(int c) {
+        return c >= ASCII.A && c <= ASCII.Z;
     }
-    
-    private static boolean isLowerCaseOutOfRange(int charCode, int shift) {
-        return charCode >= Letters.a && charCode <= Letters.z && 
-               (charCode + shift > Letters.z || charCode - shift < Letters.a);
+
+    private static boolean isLowerCaseLetter(int c) {
+        return c >= ASCII.a && c <= ASCII.z;
     }
-    
-    private static boolean isOutOfAlphabet(int charCode, int shift) {
-        return isUpperCaseLetterOutOfRange(charCode, shift) || 
-               isLowerCaseOutOfRange(charCode, shift);
+
+    private static int adjustShiftForOutofRange(int currentChar, int shift) {
+        if (isUpperCaseLetter(currentChar)) {
+            if (currentChar + shift > ASCII.Z) {
+                return shift - ALPHABET_LENGTH;
+            }
+            if (currentChar + shift < ASCII.A) {
+                return shift + ALPHABET_LENGTH;
+            }
+        } else if (isLowerCaseLetter(currentChar)) {
+            if (currentChar + shift > ASCII.z) {
+                return shift - ALPHABET_LENGTH;
+            }
+            if (currentChar + shift < ASCII.a) {
+                return shift + ALPHABET_LENGTH;
+            }
+        }
+        return shift;
     }
-    
+
+    private static String applyShift(String text, int shift) {
+        StringBuilder result = new StringBuilder();
+        int shiftToApply = shift % ALPHABET_LENGTH;
+
+        for (int i = 0; i < text.length(); i++) {
+            int currentChar = (int) text.charAt(i);
+            int finalShift = adjustShiftForOutofRange(currentChar, shiftToApply);
+            char newCharToAdd = (char) (currentChar + finalShift);
+            result.append(newCharToAdd);
+        }
+
+        return result.toString();
+    }
+
     public static String cipher(String text, int shift) {
-        StringBuilder cipher = new StringBuilder();
-        char newCharToAddToCipher;
-        int shiftToApply, currentChar;
-        shift = shift % ALPHABET_LENGTH;
-        
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = (int) text.charAt(i);
-            shiftToApply = isOutOfAlphabet(currentChar, shift) ? 
-                          shift - ALPHABET_LENGTH : shift;
-            newCharToAddToCipher = (char) (currentChar + shiftToApply);
-            cipher.append(newCharToAddToCipher);
-        }
-        return cipher.toString();
+        return applyShift(text, shift);
     }
-    
+
     public static String decipher(String text, int shift) {
-        StringBuilder decipher = new StringBuilder();
-        char newCharToAddToDecipher;
-        int shiftToApply, currentChar;
-        shift = -shift % ALPHABET_LENGTH;
-        
-        for (int i = 0; i < text.length(); i++) {
-            currentChar = (int) text.charAt(i);
-            shiftToApply = isOutOfAlphabet(currentChar, shift) ? 
-                          shift + ALPHABET_LENGTH : shift;
-            newCharToAddToDecipher = (char) (currentChar + shiftToApply);
-            decipher.append(newCharToAddToDecipher);
-        }
-        return decipher.toString();
+        return applyShift(text, -shift);
     }
-    
+
     public static void main(String[] args) {
         // Test 1
         String result1 = cipher("Hello World", 1);
         String expected1 = "Ifmmp!Xpsme";
-        assert result1.equals(expected1) : 
-            String.format("%s === '%s'", result1, expected1);
-        
+        assert result1.equals(expected1) : String.format("%s === '%s'", result1, expected1);
+
         // Test 2
         String ciphered = cipher("Hello World", 3);
         String result2 = decipher(ciphered, 3);
         String expected2 = "Hello World";
-        assert result2.equals(expected2) : 
-            String.format("%s === '%s'", result2, expected2);
-        
+
+        assert result2.equals(expected2) : String.format("%s === '%s'", result2, expected2);
+
         System.out.println("Todos los tests han pasado correctamente");
     }
 }
